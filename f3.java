3c40d2058c48415fc20607f78f0f1547f65921ae
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-24 18:03:16 +0530

    Add files via upload

---
+++ f3.java
@@ -0,0 +1,12 @@
+class If3{
+public static void main(String[] args){
+int num1=2;
+int num2=3;
+if(num1 <  num2)
+
+System.out.println("num1 is less than num2");
+}
+else {
+System.out.println("num2 is greater than num1");
+}
+}

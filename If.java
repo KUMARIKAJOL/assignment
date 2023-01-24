86db7184d216760499b4c6b85a91f7d48157030e
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-24 17:20:03 +0530

    Add files via upload

---
+++ If.java
@@ -0,0 +1,10 @@
+class If2 {
+public static void main(String[] args){
+int number = 10; 
+if (nuber < 0 ) {
+System.out.println("The number is negative.");
+}
+else {
+System.out.println("The number is positive.");
+}
+}
---
+++ If2.java
@@ -0,0 +1,11 @@
+class If2 {
+public static void main(String[] args){
+int number = 10; 
+if (number < 0 ) {
+System.out.println("The number is negative.");
+}
+else {
+System.out.println("The number is positive.");
+}
+}
+}

6c36119d3087b7b694247106ed90ba8ca3d1907e
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-31 16:18:18 +0530

    Add files via upload

---
+++ Diamondpattern.java
@@ -0,0 +1,41 @@
+package controlstatement;
+
+public class Diamondpattern {
+	public static void main(String[] args) {
+		
+	
+	for(int i=1;i<=5; i++) //raws
+	{
+		for(int j=5;j>=i; j--) // space column
+		{
+			System.out.print(" ");
+		}
+		for(int k=1;k<=2*i-1; k++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+	for(int i=5;i>=1; i--) //raws
+	{
+		for(int j=5;j>=i; j--) // space column
+		{
+			System.out.print(" ");
+		}
+		for(int k=1;k<=2*i-1; k++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
+	
+	
+	
+	
+		
+		
+		
+	

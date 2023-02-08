c4e360b44ba9a814270b9e3a319eb0ccac64fe13
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-02-08 17:03:14 +0530

    Add files via upload

---
+++ Faceprogram.java
@@ -0,0 +1,13 @@
+package kajol.com;
+
+public class Faceprogram {
+	public static void main(String[] args) {
+		System.out.println(" +\"\"\"\"\"+ ");
+		System.out.println("[| o o |]");
+		System.out.println(" |  ^  | ");
+		System.out.println(" | '-' |");
+		System.out.println(" +-----+");
+	}
+		
+		
+	}

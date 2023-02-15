d71cd2d7c7020d24660126b96b425c9534018c39
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-02-15 17:43:59 +0530

    Add files via upload

---
+++ MyProgram1.java
@@ -0,0 +1,21 @@
+class DisplayMessage
+{
+  void printMessage(){
+ 
+  System.out.println("Hello My Message");
+
+}
+
+
+class  MyProgram1
+  {
+    
+public static void main(String arg[]){
+   
+   DisplayMessage program=new DisplayMessage();
+    
+  program.printMessage();
+  
+ }
+}
+}
